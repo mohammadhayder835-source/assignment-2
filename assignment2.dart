5b4ab3e791fc0ef02f3d809ceb73b4e6a54278bb
mohammadhayder <mohammadhayder835@gmail.com>
2026-08-27 15:24:40 +0500

    assignment2

--- assignment2.dart
+++ assignment2.dart
@@ -18,12 +18,12 @@ void main(){
     int age1= 20;
     int age2= 18;
 
-    if(age1<age2){
+    if(age1>age2){
         print("age1 is older");
         print("age2 is younger");
     }
 
-    else if(age2<age1){
+    else if(age2>age1){
         print("age1 is younger");
         print("age2 is older");
     } 
@@ -41,6 +41,13 @@ void main(){
      
      print("percentage of class attended: $percentageofclassattended");
 
+     if(percentageofclassattended>=75){
+        print("he/she is allowed to sit in exam");
+     }
+
+     else{
+        print("he/she is not allowed to sit in exam");
+     }
    print("\n====question4====");
 
    int year=2000;
@@ -56,8 +63,8 @@ void main(){
    }
 
    else{
-    print("this is not not leap year");
-   }
+    print("$year is not not leap year");
+
 
    print("\n====question5====");
 
@@ -66,22 +73,22 @@ void main(){
    if(temperature<0){
     print("freezing point");
    }
-   else if(temperature<10){
+   else if(temperature>=0 && temperature<10){
    print("very cold weather");
    }
-   else if(temperature<20){
+   else if(temperature>=10 &&temperature<20){
     print("cold weather");
    }
-   else if(temperature<30){
+   else if(temperature>=20 &&temperature<30){
     print("normal temperature");
    }
-   else if(temperature<40){
+   else if(temperature>=30 && temperature<40){
     print("its hot");
    }
-   else{
+   else {
     print("its very hot");
    }
-
+   
    print("\n====question6====");
 
    String alphabet= "a";
@@ -95,9 +102,9 @@ void main(){
 
    print("\n====question7====");
 
-   int customerid= 103;
-   String name= "ali";
-   int units= 400;
+   int customerid= 1001;
+   String name= "james";
+   int units= 800;
    double billamount;
 
    if(units<=199){
@@ -110,11 +117,79 @@ void main(){
     billamount= units*1.80;
    }
    else{
-    billamount= units*200;
+        billamount= units*2.00;
    }
+
    print("customerid: $customerid");
    print("name: $name");
    print("units: $units");
-   print("billamount: ${billamount.toStringAsFixed(2)}");
+   print("Netbillamount: ${billamount.toStringAsFixed(2)}");
 
+
+
+  print("\n====question8====");
+
+  List names=["ali","aun","haider","kamran"];
+  print("names:$names");
+
+  print("\n====question9====");
+
+  List day=[];
+  day.add("monday");
+  day.add("tuesday");
+  day.add("wednesday");
+  day.add("thursday");
+  day.add("friday");
+  day.add("saturday");
+  day.add("sunday");
+
+  print("all days: $day");
+
+  print("\n====question10====");
+
+  List days=["monday","tuesday","wednesday","thursday","friday","saturday","sunday"];
+
+  days.remove("sunday");
+  days.remove("saturday");
+  days.remove("friday");
+  days.remove("thursday");
+  days.remove("wednesday");
+  days.remove("tuesday");
+  days.remove("monday");
+
+  print("days after removing: $days");
+
+  print("\n====question11====");
+
+  List number=[12,34,67,87,4,53,6,34];
+  int smallest= number[0];
+  int greatest= number[0];
+
+  for(int numbers in number){
+  if(numbers<smallest){
+    smallest=numbers;
+  }
+  
+  if(numbers>greatest){
+    greatest=numbers;
+  }
+}
+
+ print("greatest : $greatest");
+ print("smallest : $smallest");
+
+
+print("\n====question12====");
+
+List number1=[23,5,45,23,87,98];
+
+int maximam=number1[0];
+
+for(int number in number1){
+    if(number>maximam){
+     maximam=number;
+
+    }
+}
+print("maximamvalue: $maximam");
 }
